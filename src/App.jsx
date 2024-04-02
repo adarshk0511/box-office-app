4eedafcfdf1d7d4a1ad758ce530347a57c133005
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2024-04-03 01:14:41 +0530

    fetch show data"installed react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
@@ -32,6 +37,7 @@ function App() {
       <Route path="contact-us" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,27 +1,16 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getShowById } from "../api/tvmaze";
+import { QueryClientProvider, useQuery } from "@tanstack/react-query";
+
 
 const Show = () => {
 
     const {showId} = useParams();
-
-    const [showData, setShowData]=useState(null);
-    const [showError, setShowError] = useState(null);
-
-    useEffect(()=>{
-        async function fetchData(){
-            try {
-                const data = await getShowById(showId);
-                setShowData(data);
-            } catch (error) {
-                setShowError(error);
-            }
-            
-        }
-        fetchData();
-    },[showId]);
-
+    const {data: showData, error: showError} = useQuery({queryKey: ['show', showId],
+        queryFn: ()=> getShowById(showId)
+    });
+   
     if(showError){
         return <div>We have an error {showError.message}</div>
     }
