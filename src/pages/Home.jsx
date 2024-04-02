a34bff910ec875f8ce7e0c82875960c2ca12328a
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2024-04-02 17:24:01 +0530

    Added Search Box and Submit Button with TVMAZE URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () =>{
+    const [searchStr,setSearchStr] = useState("");
+
+    const onSearchInputChange = (ev) => {
+        setSearchStr(ev.target.value);
+    }
+
+    const onSearch = async (ev) => {
+        ev.preventDefault();
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
+        const body = await response.json();
+    };
+
     return (
-        <div> 
-           Home
-        </div>
+      <div>
+        <form onSubmit={onSearch}>
+          <input type="text" value={searchStr} onChange={onSearchInputChange} />
+          <button type="submit">Search !!</button>
+        </form>
+      </div>
     );
 }
 
